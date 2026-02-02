a6f77e7ee8dce25be420c233ae12ccb7ed69bda1
protodroid88 <jonathang7395@gmail.com>
2026-02-02 00:47:40 -0600

    se modifican nvim y xfce

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,30 +1,24 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
---
--- Add any additional autocmds here
--- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
--- -- Quitar fondo (transparente)
-vim.api.nvim_create_autocmd("ColorScheme", {
-  callback = function()
-    local groups = {
-      "Normal",
-      "NormalNC",
-      "NormalFloat",
-      "FloatBorder",
-      "SignColumn",
-      "EndOfBuffer",
-      "MsgArea",
-      "NeoTreeNormal",
-      "NeoTreeNormalNC",
-      "TelescopeNormal",
-      "WhichKeyFloat",
-    }
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
 
-    for _, group in ipairs(groups) do
-      vim.api.nvim_set_hl(0, group, { bg = "none" })
-    end
-  end,
-})
+      -- autocmds aquí dentro (ya combinados)
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*",
+        callback = function()
+          -- tu código aquí
+        end,
+      })
+    end,
+  },
+}
+
--- nvim/lua/config/transparency.lua
+++ nvim/lua/config/transparency.lua
@@ -20,3 +20,56 @@ return {
     },
   },
 }
+-- ~/.config/nvim/lua/user/transparency.lua
+local set_transparency = function()
+  vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+  vim.cmd("hi NormalNC guibg=NONE ctermbg=NONE")
+  vim.cmd("hi FloatBorder guibg=NONE")
+  vim.cmd("hi NormalFloat guibg=NONE")
+end
+
+-- Aplica al iniciar cualquier colorscheme
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = set_transparency,
+})
+
+-- Aplica al entrar a Neovim
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = set_transparency,
+  })
+  -- ~/.config/nvim/lua/config/transparency.lua
+
+local set_transparency = function()
+  -- Fondo principal
+  vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+  vim.cmd("hi NormalNC guibg=NONE ctermbg=NONE")
+
+  -- Ventanas flotantes
+  vim.cmd("hi NormalFloat guibg=NONE")
+  vim.cmd("hi FloatBorder guibg=NONE")
+
+  -- LSP Hover / Signature
+  vim.cmd("hi LspInfoBorder guibg=NONE")
+  vim.cmd("hi LspFloatWinNormal guibg=NONE")
+
+  -- Telescope
+  vim.cmd("hi TelescopeNormal guibg=NONE")
+  vim.cmd("hi TelescopeBorder guibg=NONE")
+
+  -- NvimTree o Sidebar
+  vim.cmd("hi NvimTreeNormal guibg=NONE")
+  vim.cmd("hi NvimTreeNormalNC guibg=NONE")
+end
+
+-- Aplica al iniciar cualquier colorscheme
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = set_transparency,
+})
+
+-- Aplica al entrar a Neovim
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = set_transparency,
+})
+
